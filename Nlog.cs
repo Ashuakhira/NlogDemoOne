526a4c7e0d75087a69443a41a021fd6aae94c602
Ashuakhira <bashok592@gmail.com>
2023-05-04 22:07:15 +0530

    Branch is  added

---
+++ Nlog.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NlogDemo
+{
+    public class Nlog
+    {
+        public static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebugTheMessage(string message)
+        {
+            logger.Debug(message);
+        }
+        public void LogErrorThemessage(string message)
+        {
+            logger.Error(message);
+        }
+        public void LogInfoTheMessage(string message)
+        {
+            logger.Info(message);
+
+
+        }
+    }
+}
