52ac45a2f1a7f479874adb4c7b65eee81057c550
Ashuakhira <bashok592@gmail.com>
2023-05-04 22:05:25 +0530

    Nlog Demo added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NlogDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            sum.AddTheNumbers(10, 20);
+            // Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}
